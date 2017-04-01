718c7da2030b99fca506120b1d9b3a5fce34244a
Eric Cramer <ecramer@gaikai.com>
2017-03-31 21:50:27 -0700

    Added help

--- freecell.py
+++ freecell.py
@@ -26,7 +26,15 @@ class FreeCell(Problem):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument('filename', help=(
+        'A csv file where each row represents a pile '
+        'from top to bottom.'
+        ' Each card is a two-character string (e.g. "3C").'
+        ' Non-single-digit cards are one of these strings: '
+        '"A", "T", "J", "Q", and "K". The suits are "H", '
+        '"D", "C", and "S". All letters may be upper or '
+        'lower case.'
+    ))
     args = parser.parse_args()
 
     freecell_prob = FreeCell(args.filename)
